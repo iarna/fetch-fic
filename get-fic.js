70e1b34f5428e432fdb3097a819117831d220be5
Rebecca Turner <me@re-becca.org>
2016-09-20 01:03:48 -0700

    Rewrite fic-stream to better honor backpressure

--- get-fic.js
+++ get-fic.js
@@ -7,20 +7,17 @@ var inherits = require('util').inherits
 
 function getFic (fetch, chapterList, maxConcurrency) {
   if (!maxConcurrency) maxConcurrency = 4
-  var fic = new FicStream()
-  Bluebird.map(chapterList, function (chapterInfo, ii) {
+  var fic = new FicStream({highWaterMark: maxConcurrency})
+  Bluebird.each(chapterList, function (chapterInfo, ii) {
     return getChapter(fetch, chapterInfo.link).then(function (chapter) {
       chapter.order = chapterInfo.order
       chapter.name = chapterInfo.name
-      return chapter
+      return fic.queueChapter(chapter)
     }).catch(function (err) {
       console.error(err.message)
     })
-  }, {concurrency: maxConcurrency}).each(function (chapter) {
-    if (!chapter) return
-    fic.queueChapter(chapter)
-  }).then(function () {
-    fic.queueChapter(null)
+  }, {concurrency: maxConcurrency}).finally(function () {
+    return fic.queueChapter(null)
   })
   return fic
 }
@@ -28,17 +25,29 @@ function getFic (fetch, chapterList, maxConcurrency) {
 function FicStream (options) {
   if (!options) options = {}
   options.objectMode = true
-  options.highWaterMark = 4
+  if (!options.highWaterMark) options.highWaterMark = 4
   Readable.call(this, options)
   this.FicStream = { reading: false, chapterBuffer: [] }
+  this.readyP = null
+  this.readyR = null
 }
 inherits(FicStream, Readable)
 
 FicStream.prototype.queueChapter = function (chapter) {
   if (this.FicStream.reading) {
     this.FicStream.reading = this.push(chapter)
+    if (chapter == null) return Bluebird.resolve()
   } else {
     this.FicStream.chapterBuffer.push(chapter)
+  }
+  if (this.FicStream.reading) {
+    return null
+  } else {
+    if (this.readyP) return this.readyP
+    var self = this
+    return this.readyP = new Bluebird(function (resolve) {
+      self.readyR = resolve
+    })
   }
 }
 
@@ -47,5 +56,9 @@ FicStream.prototype._read = function (size) {
   while (this.FicStream.reading && this.FicStream.chapterBuffer.length) {
     var chapter = this.FicStream.chapterBuffer.shift()
     this.FicStream.reading = this.push(chapter)
+  }
+  if (this.FicStream.reading && this.readyP) {
+    this.readyR()
+    this.readyR = this.readyP = null
   }
 }
