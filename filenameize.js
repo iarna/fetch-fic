bd9c3c4092b3070413c8757f6b777042349642f9
Rebecca Turner <me@re-becca.org>
2016-09-26 17:56:25 -0700

    Ooops, remove all the supurfluous dashes

--- filenameize.js
+++ filenameize.js
@@ -2,5 +2,5 @@
 module.exports = filenameize
 
 function filenameize (str) {
-  return str.replace(/\W/g, '-').replace(/--+/g, '-').replace(/^-|-$/, '').toLowerCase()
+  return str.replace(/\W/g, '-').replace(/--+/g, '-').replace(/^-|-$/g, '').toLowerCase()
 }
