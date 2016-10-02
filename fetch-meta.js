d87ac20385703670a2a04e1148fcaea907957c0f
Rebecca Turner <me@re-becca.org>
2016-10-01 23:34:36 -0700

    Only abort early if we're updating

--- fetch-meta.js
+++ fetch-meta.js
@@ -124,7 +124,7 @@ function main () {
       } else {
         fic.chapters = newChapters
       }
-      if (!actions.length) return null
+      if (!actions.length && filename) return null
       if (!filename) filename = filenameize(fic.title) + '.fic.toml'
       fs.writeFileSync(filename, TOML.stringify(fic))
       process.stdout.write(filename + '\n')
