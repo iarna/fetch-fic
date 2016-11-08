59ad3cfea8d3d324134c14cc923aaaf31732a554
Rebecca Turner <me@re-becca.org>
2016-11-08 10:01:36 -0800

    Better qw

--- qw.js
+++ qw.js
@@ -1,7 +1,19 @@
 'use strict'
 module.exports = qw
 
-function qw (args) {
-  var template = args[0]
-  return template.replace(/^\s+|\s+$/g, '').replace(/\s+/g, ' ').split(/ /)
+function qw () {
+  const args = Object.assign([], arguments[0])
+  const values = [].slice.call(arguments, 1)
+  const words = []
+  while (args.length) {
+    const arg = args.shift()
+    if (arg.trim() !== '') {
+      words.push.apply(words, arg.replace(/^\s+|\s+$/g, '').replace(/\s+/g, ' ').split(/ /))
+    }
+    if (values.length) {
+      const val = values.shift()
+      words.push(val)
+    }
+  }
+  return words
 }
