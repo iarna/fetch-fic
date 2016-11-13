8f86a9145f482bad174af33ff86fd18b691f4c3b
Rebecca Turner <me@re-becca.org>
2016-11-13 04:45:16 -0800

    fic-to-epub: Better default title page

--- fic-to-epub.js
+++ fic-to-epub.js
@@ -6,6 +6,8 @@ var sanitizeHtml = require('sanitize-html')
 var ms = require('mississippi')
 var url = require('url')
 var fs = require('fs')
+var commaNumber = require('comma-number')
+var html = require('html-template-tag')
 
 function ficToEpub (meta) {
   var epub = new Streampub({
@@ -23,13 +25,40 @@ function ficToEpub (meta) {
   if (meta.cover) {
     epub.write(Streampub.newCoverImage(fs.createReadStream(meta.cover)))
   } else {
-    var title =
-      '<div style="text-align: center;">' +
-      '<h1>' + meta.title + '</h1>' +
-      '<h3>' + meta.author + '</h3>' +
-      '<p>URL: ' + '<a href="' + meta.link + '">' + meta.link + '</a></p>' +
-      '</div>'
-    epub.write(Streampub.newChapter('Title Page', title, 0, 'top.xhtml'))
+    let titleContent = ''
+    titleContent += html`
+<head>
+<title>${meta.title}</title>
+<style>
+  h1, h3 { text-align: center; }
+  table {
+    border: 3px double #ccc;
+    padding: 0.5em;
+  }
+  th {
+    text-align: right;
+    font-weight: bold;
+    text-decoration: underline;
+    white-space: nowrap;
+    vertical-align: top;
+  }
+  th:after {
+    content: ":";
+  }
+</style></head>`
+    titleContent += html`<h1>${meta.title}</h1>`
+    const author = meta.authorUrl ? html`<a href="${meta.authorUrl}">${meta.author}</a>` : meta.author
+    titleContent += `<h3>by ${author}</h3>`
+    titleContent += html`<table>`
+    titleContent += html`<tr><th>Source</th><td><a href="${meta.link}">${meta.link}</a></td></tr>`
+    if (meta.created) titleContent += html`<tr><th>Published</th><td>${meta.created}</td></tr>`
+    if (meta.modified) titleContent += html`<tr><th>Updated</th><td>${meta.modified}</td></tr>`
+    if (meta.tags && meta.tags.length) titleContent += html`<tr><th>Tags</th><td><em>${meta.tags.join(', ')}</em></td></tr>`
+    if (meta.words) titleContent += html`<tr><th>Words</th><td>${commaNumber(meta.words)}</td></tr>`
+    titleContent += `</table>`
+    if (meta.description) titleContent += `<div>${meta.description}</div>`
+    var titlePage = `${titleContent}`
+    epub.write(Streampub.newChapter('Title Page', titlePage, 0, 'top.xhtml'))
   }
   return ms.pipeline.obj(ms.through.obj(transformChapter(meta)), epub)
 }
