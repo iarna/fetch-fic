f26cfd98218736814b8b30f44b0ad45f838ff89e
Rebecca Turner <me@re-becca.org>
2016-11-17 23:29:57 -0800

    Add generic image support

---
+++ site-generic-image.js
@@ -0,0 +1,43 @@
+'use strict'
+const Site = require('./site.js')
+const cheerio = require('cheerio')
+const Bluebird = require('bluebird')
+const url = require('url')
+
+class GenericImage extends Site {
+  static matches (siteUrlStr) {
+    return !/clear.png/.test(siteUrlStr) && /[.](?:jpg|jpeg|png|gif|svg)$/.test(siteUrlStr)
+  }
+
+  constructor (siteUrlStr) {
+    super(siteUrlStr)
+    this.publisher = url.parse(siteUrlStr).hostname
+    this.publisherName = this.publisher
+  }
+
+  getFicMetadata (fetch, fic) {
+    fic.title = this.link
+    fic.link = this.link
+    fic.publisher = this.publisher
+    // currently we only support /art/ urls, which can only have one thing on them
+    return this.getChapter(fetch, this.link).then(info => {
+      fic.link = this.normalizeLink(info.finalUrl)
+      fic.addChapter(this.link, fic.link)
+    })
+  }
+
+  scrapeFicMetadata (fetch, fic) {
+    // There's never any reason to scrape Divant Art content.
+    return Bluebird.resolve()
+  }
+
+  getChapter (fetch, chapter) {
+    return Bluebird.resolve({
+      meta: chapter,
+      finalUrl: chapter,
+      base: chapter,
+      content: `<img src="${chapter}">`
+    })
+  }
+}
+module.exports = GenericImage
