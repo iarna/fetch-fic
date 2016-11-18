1c519f8a435c287227187195c0362ca356ffe6f4
Rebecca Turner <me@re-becca.org>
2016-11-17 16:04:06 -0800

    qw-ify the array literals

--- site.js
+++ site.js
@@ -29,24 +29,21 @@ class Site {
   sanitizeHtmlConfig () {
     return {
       // from: https://www.amazon.com/gp/feature.html/ref=amb_link_357754562_1?ie=UTF8&docId=1000729901&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=center-10&pf_rd_r=P6ATRSS3E2FJJ5ME5QR2&pf_rd_t=1401&pf_rd_p=1343223442&pf_rd_i=1000729511
-      allowedTags: [
-        'a', 'address', 'article', 'aside', 'b', 'blockquote', 'body', 'br',
-        'caption', 'center', 'cite', 'code', 'col', 'dd', 'del', 'dfn', 'div',
-        'dl', 'dt', 'em', 'figcaption', 'figure', 'footer', 'h1', 'h2', 'h3',
-        'h4', 'h5', 'h6', 'head', 'header', 'hgroup', 'hr', 'html', 'i', 'img',
-        'ins', 'kbd', 'li', 'link', 'mark', 'menu', 'ol', 'output', 'p', 'pre',
-        'q','rp', 'rt', 'samp', 'section', 'small', 'source', 'span', 'strong',
-        'style', 'strike', 'sub', 'sup', 'table', 'tbody', 'td', 'tfoot', 'th',
-        'thead', 'time', 'title', 'tr', 'u','ul', 'var', 'wbr', 'nav', 'summary'
-      ],
+      allowedTags: qw`
+        a address article aside b blockquote body br caption center cite
+        code col dd del dfn div dl dt em figcaption figure footer h1 h2 h3
+        h4 h5 h6 head header hgroup hr html i img ins kbd li link mark menu
+        ol output p pre q rp rt samp section small source span strong style
+        strike sub sup table tbody td tfoot th thead time title tr u ul var
+        wbr nav summary`,
       allowedAttributes: {
-        img: [ 'src', 'width', 'height', 'alt' ],
-        a: [ 'href', 'name', 'target' ],
-        '*': [ 'style' ]
+        img: qw`src width height alt`,
+        a: qw`href name target`,
+        '*': qw`style`
       },
-      allowedSchemes: [ 'http', 'https' ],
+      allowedSchemes: qw`http https`,
       allowedSchemesByTag: {
-        img: [ 'data', 'http', 'https' ]
+        img: qw`data http https`
       },
       parser: {
         lowerCaseAttributeNames: true
