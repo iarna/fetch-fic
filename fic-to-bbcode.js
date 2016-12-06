da13ce85f90483d5cf2042be24a7613bcc2586f5
Rebecca Turner <me@re-becca.org>
2016-12-06 12:16:05 -0800

    fic-to-bbcode: Better bbcode indexes

--- fic-to-bbcode.js
+++ fic-to-bbcode.js
@@ -34,33 +34,36 @@ function transformChapter (fic, dirname, ready) {
 }
 
 function writeIndex (fic, dirname) {
-  let index = `[center][size=200]${fic.title}[/size][/center]\n\n`
-  if (fic.cover) index += `[img]${fic.cover}[/img]\n`
-  if (fic.description) index += `${bbencode(fic.description)}\n\n`
-  if (fic.created) index += `First Published: ${fic.created}\n`
-  if (fic.modified) index += `Last Updated: ${fic.modified}\n`
-  if (fic.tags && fic.tags.length) index += `Tags: [i]${fic.tags.join(', ')}[/i]\n`
-  if (fic.words) index += `Words: ${commaNumber(fic.words)}\n`
-  index += '\n[list]\n'
-  for (let chapter of fic.chapters) {
-    index += `[*] [url=${chapter.link}]${chapter.name}[/url]`
-    const author = chapter.author || fic.author
-    const authorUrl = chapter.authorUrl || fic.authorUrl
-    if (author !== fic.author) {
-      const authorBB = authorUrl
-        ? `[url=${authorUrl}]${author}[/url]`
-        : author
-      index += ` (${authorBB})`
+  const indexHTML = Promise.resolve(fic.description && HTMLToBBCode(fic.description)).then(ficDescription => {
+    let index = `[center][b][size=7]${fic.title}[/size][/b][/center]\n\n`
+    if (fic.cover) index += `[img]${fic.cover}[/img]\n`
+    if (ficDescription) index += `${ficDescription}\n\n`
+    if (fic.created) index += `First Published: ${fic.created}\n`
+    if (fic.modified) index += `Last Updated: ${fic.modified}\n`
+    if (fic.tags && fic.tags.length) index += `Tags: [i]${fic.tags.join(', ')}[/i]\n`
+    if (fic.words) index += `Words: ${commaNumber(fic.words)}\n`
+    index += '\n[list]\n'
+    for (let chapter of fic.chapters) {
+      index += `[*] [url=${chapter.link}]${chapter.name}[/url]`
+      const author = chapter.author || fic.author
+      const authorUrl = chapter.authorUrl || fic.authorUrl
+      if (author !== fic.author) {
+        const authorBB = authorUrl
+          ? `[url=${authorUrl}]${author}[/url]`
+          : author
+        index += ` (${authorBB})`
+      }
+      if (chapter.description) {
+        index += ' – ' + chapter.description
+      }
+      if (chapter.words) {
+        index += ` (${chapter.words} words)`
+      }
+      index += `\n`
     }
-    if (chapter.description) {
-      index += ' – ' + chapter.description
-    }
-    if (chapter.words) {
-      index += ` (${chapter.words} words)`
-    }
-    index += `\n`
-  }
-  index += '[/list]\n'
+    index += '[/list]\n'
+    return index
+  })
 
-  return writeFile(path.join(dirname, 'index.bbcode'), index)
+  return writeFile(path.join(dirname, 'index.bbcode'), indexHTML)
 }
