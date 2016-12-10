02c423b36f75c9032622baa7fcc69babfce54cf8
Rebecca Turner <me@re-becca.org>
2016-12-10 03:56:44 -0800

    promisify: Allow our promisify to bind methods

--- promisify.js
+++ promisify.js
@@ -2,19 +2,19 @@
 module.exports = promisify
 const Bluebird = require('bluebird')
 
-function promisify (fn) {
+function promisify (fn, bind) {
   const pfn = Bluebird.promisify(fn)
   return function () {
-    const self = this
+    const self = bind || this
     return Bluebird.all(arguments).spread(function () {
       return pfn.apply(self, arguments)
     })
   }
 }
 
-promisify.sync = function (fn) {
+promisify.sync = function (fn, bind) {
   return function () {
-    const self = this
+    const self = bind || this
     return Bluebird.all(arguments).spread(function () {
       return fn.apply(self, arguments)
     })
