3d85c79dcaf08dd6cedce590533ffe67579301d6
Rebecca Turner <me@re-becca.org>
2017-05-16 11:30:04 -0700

    rtf-to-html: Force HTML5 doctype (and thus utf8)

--- util/rtf-to-html.js
+++ util/rtf-to-html.js
@@ -3,5 +3,5 @@ const promisify = use('promisify')
 const rtfToHTML = promisify(require('@iarna/rtf-to-html').fromString)
 
 module.exports = rtf => rtfToHTML(rtf).then(html => {
-  return html.replace(/<[/](\w+)><\1>/g, '')
+  return '<!DOCTYPE html>' + html.replace(/<[/](\w+)><\1>/g, '')
 })
