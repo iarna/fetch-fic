a0c43e32c4d363eb5c4fc1ee1b5d863c3c4c8f17
Rebecca Turner <me@re-becca.org>
2017-05-19 21:45:39 -0700

    html-template-tag: Expand newlines

--- util/html-template-tag.js
+++ util/html-template-tag.js
@@ -5,13 +5,13 @@ module.exports = function (literals) {
   if (!literals.raw) return htmlEscape(literals)
   const substs = [].slice.call(arguments, 1)
   return literals.raw.reduce((acc, lit, i) => {
-    let subst = substs[i - 1]
+    let subst = i > 0 ? substs[i - 1] : ''
     if (Array.isArray(subst)) {
       subst = subst.join('')
     } else {
       subst = htmlEscape(subst)
     }
 
-    return acc + subst + lit
-  })
+    return acc + subst + lit.replace(/\\n/g, '\n')
+  }, '')
 }
