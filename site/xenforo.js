1df40713a89f71b23f99e50bcdcb10c1f2730b41
Rebecca Turner <me@re-becca.org>
2017-06-20 14:43:11 -0700

    xenforo: Match threadmarks from short thread links

--- site/xenforo.js
+++ site/xenforo.js
@@ -358,7 +358,7 @@ class Xenforo extends Site {
   threadmarkUrl () {
     const threadUrl = url.parse(this.raw)
     const path = threadUrl.pathname || threadUrl.path
-    const threadMatch = /^([/]threads[/][^/]+\.\d+)(?:[/].*)?$/
+    const threadMatch = /^([/]threads[/](?:[^/]+\.)?\d+)(?:[/].*)?$/
     if (threadMatch.test(path)) {
       threadUrl.hash = ''
       threadUrl.pathname = threadUrl.pathname.replace(threadMatch, '$1/threadmarks')
