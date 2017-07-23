700ae3189e559dcd4b74fa714193c9c366b9763b
Rebecca Turner <me@re-becca.org>
2017-07-22 17:50:39 -0700

    promisify: use util.promisify, what could go wrong?
    Possibly dangerous due to util.promisify not working like a generic
    promisifier.

--- util/promisify.js
+++ util/promisify.js
@@ -1,16 +1,14 @@
 'use strict'
 module.exports = promisify
-
+const util = require('util')
 const Bluebird = require('bluebird')
 
 function promisify (fn, bind) {
+  const raw = util.promisify(fn)
   return function () {
     const self = bind || this
     return Bluebird.all(arguments).then(args => {
-      return new Bluebird((resolve, reject) => {
-        args.push((err, value) => err ? reject(err) : resolve(value))
-        return fn.apply(self, args)
-      })
+      return raw.apply(self, args)
     })
   }
 }
