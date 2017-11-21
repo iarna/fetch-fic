0d7dd98c55b1a26fa53f57f24588992b8856512e
Rebecca Turner <me@re-becca.org>
2017-11-21 14:34:06 -0800

    rtf-to-html: asyncify

--- util/rtf-to-html.js
+++ util/rtf-to-html.js
@@ -2,6 +2,7 @@
 const promisify = use('promisify')
 const rtfToHTML = promisify(require('@iarna/rtf-to-html').fromString)
 
-module.exports = rtf => rtfToHTML(rtf).then(html => {
+module.exports = async rtf => {
+  const html = await rtfToHTML(rtf)
   return '<!DOCTYPE html>' + html.replace(/<[/](\w+)><\1>/g, '')
-})
+}
