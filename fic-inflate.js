bb8353439f2c3ccd4812f84d2a8238f6b5df0c3f
Rebecca Turner <me@re-becca.org>
2018-12-04 05:20:07 +0000

    fic-inflate: Let work w/o progress indicator

--- fic-inflate.js
+++ fic-inflate.js
@@ -10,39 +10,46 @@ const progress = use('progress')
 // is fully complete.
 
 function ficInflate (fic, fetch, tracker) {
-  if (!tracker) tracker = progress.tracker
   return Bluebird.resolve(fic).then(fic => {
     const fics = [fic].concat(fic.fics)
     const completion = new Map()
-    for (let fic of fics) {
-      completion.set(fic, tracker.newItem(fic.title, fic.chapters.length))
+    if (tracker) {
+      for (let fic of fics) {
+        completion.set(fic, tracker.newItem(fic.title, fic.chapters.length))
+      }
     }
     return Bluebird.map(fics, fic => {
       const tracker = completion.get(fic)
       process.emit('debug', 'Inflating', fic.title, 'chapters:', fic.chapters.length)
       let words = 0
       return Bluebird.map(fic.chapters, chapterInfo => {
-        const chapterContent = chapterInfo.getContent(fetch)
-        return progress.completeWorkWhenResolved(chapterContent, tracker).then(chapter => {
-          process.emit('debug', `Got content for #${chapterInfo.order}: ${chapterInfo.name}`)
-          progress.show(fic.title, `${chapterInfo.name}`)
-          chapterInfo.words = chapter.words
-          chapterInfo.tags = chapter.tags
-          words += chapter.words
-          if (chapterInfo.link == null) chapterInfo.link = chapter.chapterLink
-          for (let prop of qw`name author authorUrl created modified headings`) {
-            if (chapterInfo[prop] == null) chapterInfo[prop] = chapter[prop]
-          }
-          if (chapterInfo.author === fic.author || chapterInfo.authorUrl === fic.authorUrl) {
-            fic.author = chapterInfo.author
-            fic.authorUrl = chapterInfo.authorUrl
-            chapterInfo.author = null
-            chapterInfo.authorUrl = null
-          }
-        }).catch(err => {
+        try {
+          const chapterContent = chapterInfo.getContent(fetch)
+          const resolveFrom = tracker ? progress.completeWorkWhenResolved(chapterContent, tracker) : chapterContent
+          return resolveFrom.then(chapter => {
+            process.emit('debug', `Got content for #${chapterInfo.order}: ${chapterInfo.name}`)
+            if (tracker) progress.show(fic.title, `${chapterInfo.name}`)
+            chapterInfo.words = chapter.words
+            chapterInfo.tags = chapter.tags
+            words += chapter.words
+            if (chapterInfo.link == null) chapterInfo.link = chapter.chapterLink
+            for (let prop of qw`name author authorUrl created modified headings`) {
+              if (chapterInfo[prop] == null) chapterInfo[prop] = chapter[prop]
+            }
+            if (chapterInfo.author === fic.author || chapterInfo.authorUrl === fic.authorUrl) {
+              fic.author = chapterInfo.author
+              fic.authorUrl = chapterInfo.authorUrl
+              chapterInfo.author = null
+              chapterInfo.authorUrl = null
+            }
+          }).catch(err => {
+            chapterInfo.error = err
+            process.emit('error', err.message)
+          })
+        } catch (err) {
           chapterInfo.error = err
-          process.emit('error', err.message)
-        })
+          process.emit('error', chapterInfo)
+        }
       }).finally(() => {
         if (!fic.words) fic.words = words
         if (fic.chapters.length) {
@@ -55,7 +62,7 @@ function ficInflate (fic, fetch, tracker) {
             lastChapter.modified = fic.modified
           }
         }
-        tracker.finish()
+        if (tracker) tracker.finish()
       })
     })
   }).then(() => fic)
