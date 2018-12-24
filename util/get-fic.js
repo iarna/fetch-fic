a1ac64ea4c718a34311225738f4e5d20dcd93fb1
Rebecca Turner <me@re-becca.org>
2018-12-24 21:42:55 +0000

    get-fic: Guard URL cleaning better

--- util/get-fic.js
+++ util/get-fic.js
@@ -16,14 +16,15 @@ const qr = require('@perl/qr')
 function rewriteLinks (fic, chapter, handleLink) {
   chapter.$content.find('a').each((ii, a) => {
     const $a = chapter.$content.find(a)
-    const startAs = $a.attr('href').replace(qr`^(?:https?://)?(?:\s|%20)+`, '')
-        .replace(qr`^https?://.*(https?//)`, 'https://')
-        .replace(qr`^https?//`, 'https://')
+    let startAs = $a.attr('href')
     if (!startAs) {
       $a.remove()
       return
     }
     if (startAs[0] === '#') return
+    startAs = startAs.replace(qr`^(?:https?://)?(?:\s|%20)+`, '')
+        .replace(qr`^https?://.*(https?//)`, 'https://')
+        .replace(qr`^https?//`, 'https://')
     let src
     if (startAs.substr(0,4) !== 'http' && /^www[.]|[.]com$/.test(startAs)) {
       src = 'http://' + startAs
