b6890514679111f9471c6c9614480503684479b7
Rebecca Turner <me@re-becca.org>
2019-05-02 14:15:20 -0700

    filenameize: Provide more "script" to text translations

--- util/filenameize.js
+++ util/filenameize.js
@@ -2,6 +2,21 @@
 module.exports = filenameize
 const remove = require('diacritics').remove
 
+function romanize (str) {
+  return remove(str
+    .replace(/ᔕ/g, 'S')
+    .replace(/є/g, 'e')
+    .replace(/и/g, 'n')
+    .replace(/т/g, 't')
+    .replace(/у/g, 'y')
+    .replace(/α/g, 'a')
+    .replace(/в/g, 'b')
+    .replace(/ν/g, 'v')
+    .replace(/ѕ/g, 's')
+    .replace(/я/g, 'r')
+  )
+}
+
 function filenameize (str) {
-  return remove(str.normalize('NFKC')).replace(/\W/g, '-').replace(/--+/g, '-').replace(/^-|-$/g, '').toLowerCase()
+  return romanize(str.normalize('NFKC')).replace(/\W/g, '-').replace(/--+/g, '-').replace(/^-|-$/g, '').toLowerCase()
 }
